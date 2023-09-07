63d71a3dd89b3a1c73ea3556ae5e76431d978cd4
Hazem El-Sayed <118324667+zoma0x00@users.noreply.github.com>
2023-09-07 05:10:33 +0300

    Update main.go

--- 1- Net-Scanning/1.2 - CNAME/main.go
+++ 1- Net-Scanning/1.2 - CNAME/main.go
@@ -1,1 +1,29 @@
+package main
 
+import (
+	"flag"
+	"fmt"
+	"net"
+)
+
+//Function to output the CNAME Record for a host name
+func CNAME(host string) {
+	CNAME, err := net.LookupCNAME(host)
+  // Handle the error if exist
+	if err != nil {
+		fmt.Println(err)
+		return // End CNAME function if err is exist 
+    }
+  // Return the CNAME Record if no error exist
+  fmt.Println("CNAME Record for", host, "is :", CNAME)
+	}
+	
+//The main function
+func main(){
+  // Taking the domain from the user as argument with -d flag 
+  domain := flag.String("d", "", "Domain name or host name ")
+  flag.Parse()
+  CNAME(*domain)
+  
+ 
+}
