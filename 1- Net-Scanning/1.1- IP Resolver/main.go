e79b6b0f57e2f5e4dbd6805116eb1224bf4c3b87
Hazem El-Sayed <118324667+zomasec@users.noreply.github.com>
2024-01-26 17:28:14 +0200

    Update main.go

--- 1- Net-Scanning/1.1- IP Resolver/main.go
+++ 1- Net-Scanning/1.1- IP Resolver/main.go
@@ -1,39 +1,40 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "net"
+	"flag"
+	"fmt"
+	"log"
+	"net"
 )
 
 func main() {
 
-  ipPtr := flag.String("ip", "", "IP address to lookup")
-  domainPtr := flag.String("d", "", "Domain to lookup IP for")
+	ipPtr := flag.String("ip", "", "IP address to lookup")
+	domainPtr := flag.String("d", "", "Domain to lookup IP for")
 
-  flag.Parse()
+	flag.Parse()
 
-  if *ipPtr != "" {
-    // Reverse lookup
-    ip := net.ParseIP(*ipPtr)
-    names, _ := net.LookupAddr(ip.String())
+	if *ipPtr != "" {
+		// Reverse lookup
+		ip := net.ParseIP(*ipPtr)
+		names, _ := net.LookupAddr(ip.String())
 
-    fmt.Printf("Domains/subdomains for %s:\n", ip)
-    for _, name := range names {
-      fmt.Println(name)
-    }
+		fmt.Printf("Domains/subdomains for %s:\n", ip)
+		for _, name := range names {
+			fmt.Println(name)
+		}
 
-  } else if *domainPtr != "" {
-    // Forward lookup   
-    ips, _ := net.LookupIP(*domainPtr)
+	} else if *domainPtr != "" {
+		// Forward lookup   
+		ips, _ := net.LookupIP(*domainPtr)
 
-    fmt.Printf("IP addresses for %s:\n", *domainPtr)
-    for _, ip := range ips {
-      fmt.Println(ip)
-    }
+		fmt.Printf("IP addresses for %s:\n", *domainPtr)
+		for _, ip := range ips {
+			fmt.Println(ip)
+		}
 
-  } else {
-    fmt.Println("Error: no IP or domain specified")
-  }
+	} else {
+		log.Fatalln("[Error]: no IP or domain specified")
+	}
 
 }
